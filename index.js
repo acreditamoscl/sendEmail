c69e9d3b253a77716122d2f57d66a4e0ae6a1419
acreditamoscl <112587581+acreditamoscl@users.noreply.github.com>
2022-09-01 14:12:30 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require('cors')
+const nodemailer = require("nodemailer");
+
+const app = express()
+// middlewares
+app.use(express.json());
+app.use(cors())
+app.use(express.urlencoded({extended:true}))
+app.set('port', process.env.PORT || 8040);
+
+function main(obj) {
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // use TLS
+        //FreeMail
+        //wihetkheeqvhytza
+        auth: {
+          user: "loremplayofficial@gmail.com",
+          pass: "wihetkheeqvhytza",
+        }
+      });
+    let message = {
+      from: "loremplayofficial@gmail.com",
+      ...obj
+    };
+    // send mail with defined transport object
+    transporter.sendMail(message, (err, info)=>err?console.log(err):console.log(info));
+}
+app.post("/email", (req, res)=>{
+  
+  main(req.body)
+  console.log('new cliente')
+  res.send("enviado")
+})
+
+app.listen(app.get('port'), ()=>{
+  console.log("iniciado en el puerto: "+app.get('port'))
+})
+
+// {
+//   to: "jvrbanquez@gmail.com",
+//   subject: "Message title",
+//   text: "Plaintext version of the message",
+//   html: "<p>HTML version of the message</p>"
+// }
