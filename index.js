3c63157d850f11fcf5a1bebc48ae0216ae756fce
acreditamoscl <112587581+acreditamoscl@users.noreply.github.com>
2022-09-12 12:36:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function main(obj) {
         //wihetkheeqvhytza
         auth: {
           user: "acreditamos.cl@gmail.com",
-          pass: "qlhldaruvrmvygak",
+          pass: "wdcvtzgsjxsayqvz",
         }
       });
     let message = {
